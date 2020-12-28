84f3f14edc414aae723f0c6f07fa2ea53bb43717
flexagoon <66178592+flexagoon@users.noreply.github.com>
2020-12-28 19:52:06 +0000

    refactor: большой рефакторинг всего проекта (#1)
    * refactor: использовать библиотеку Colorama
    
    Использовать библиотеку Colorama для вывода цветного текста, чтобы улучшить читаемость кода.
    
    * refactor: перенести надпись xaker в основной файл
    
    * удалить ненужный файл
    
    * feat: создать установочный скрипт
    
    * docs: использовать формат Markdown
    
    Заменить синтаксис HTML на более понятный Markdown
    
    * style: добавить остутствующий пробел
    
    * feat: добавить файл зависимостей
    
    * refactor: изменить термин "DDoS" на "DoS"
    
    Атака с одного устройства - это DoS, а не DDoS.
    
    * refactor: изменить расположение файла bomber.py
    
    * refactor: изменить расположение файла pkgfish.py
    
    * fix: добавить telethon в список зависимостей
    
    * fix: удалить ненужный файл
    
    * fix: удалить ненужный файл
    
    * refactor: изменить расположение файла tgspam.py
    
    * fix: удалить ненужный файл
    
    * fix: добавить smtlib в список зависимостей
    
    * fix: удалить ненужный файл
    
    * fix: удалить ненужный файл
    
    * refactor: изменить расположение файла email.py
    
    * fix: исправить пути к файлам

--- core/packages/email/empak.sh
+++
@@ -1,6 +0,0 @@
-pip install --upgrade pip
-pip install colorama
-pip install smtlib
-clear
-echo "Все пакеты установлены. При следующем запуске жмите [1]. Скрипт запустится"
-python ~/px/core/tools/EMAIL.py
--- xaker.py
+++ xaker.py
@@ -1,24 +1,38 @@
+from colorama import Fore
 import os
-xaker = input("""\033[32m[1]Дудос
-[2]бомбер
+
+print(Fore.BLUE + """
+               █
+               █
+               █
+ █▒ ▒█  ░███░  █  ▒█   ███    █▒██▒
+ ░█ █░  █▒ ▒█  █ ▒█   ▓▓ ▒█   ██  █
+  ▓█▓       █  █▒█    █   █   █
+  ░█░   ▒████  ██▓    █████   █
+  ███   █▒  █  █░█░   █       █
+ ░█ █░  █░ ▓█  █ ░█   ▓▓  █   █
+ █▒ ▒█  ▒██▒█  █  ▒█   ███▒   █
+made by @pkgsearch""")
+
+sel = input(Fore.GREEN + """[1] Дудос
+[2] Бомбер
 [3] Фишинг любого сайта
 [4] Спамер тг
 [5] Емаил бомбер
 Выберите параметр: """)
-if xaker == "1":
+if sel == "1":
     os.system("clear")
-    os.system("python ~/px/core/tools/DDOS.py")
-elif xaker == "2":
+    os.system("python ~/.xaker/core/DoS.py")
+elif sel == "2":
        os.system("clear") 
-       os.system("python ~/px/core/tools/BOMBER.py")
-elif xaker == "3":
+       os.system("python ~/.xaker/core/bomber.py")
+elif sel == "3":
        os.system("clear")
-       os.system("python ~/px/core/tools/PKGFISH.py")
-elif xaker == "4":
-       os.system("python ~/px/core/packages/tgspam/menutg.py")
-elif xaker == "5":
-       os.system("python ~/px/core/packages/email/menuem.py")
+       os.system("python ~/.xaker/core/pkgfish.py")
+elif sel == "4":
+       os.system("python ~/.xaker/core/tgspam.py")
+elif sel == "5":
+       os.system("python ~/.xaker/core/email.py")
 else:
     print("Выбор не корректен :(\nПопробуйте еще раз!")
     exit(0)
-    
