5f5b0a684ce4c686dd66c4fdb32050c563e8e067
pkgsearch <73498800+pkgsearch@users.noreply.github.com>
2021-01-04 17:36:19 +0300

    Update xaker.py

--- xaker.py
+++ xaker.py
@@ -6,15 +6,17 @@ xaker = input("""\033[32m[1]Дудос
 Выберите параметр: """)
 if xaker == "1":
     os.system("clear")
-    os.system("python ~/px/core/tools/DDOS.py")
+    os.system("python /core/DDOS.py")
 elif xaker == "2":
        os.system("clear") 
-       os.system("python ~/px/core/tools/BOMBER.py")
+       os.system("python /core/BOMBER.py")
 elif xaker == "3":
        os.system("clear")
-       os.system("python ~/px/core/tools/PKGFISH.py")
+       os.system("python /core/PKGFISH.py")
 elif xaker == "4":
-       os.system("python ~/px/core/packages/menutg.py")
+       os.system("python core/TGSPAM.py")
+        elif xaker == "5":
+       os.system("python core/EMAIL.py")
 else:
     print("Выбор не корректен :(\nПопробуйте еще раз!")
     exit(0)
