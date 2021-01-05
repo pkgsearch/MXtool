7b1e17969ece44248f490c072aaa835502b5b9e6
pkgsearch <73498800+pkgsearch@users.noreply.github.com>
2021-01-05 12:41:26 +0300

    Update VK.py

--- core/VK.py
+++ core/VK.py
@@ -20,8 +20,6 @@ huy = input("Вводи токен")
 token = vk_api.VkApi(token = tok) 
 vk = token.get_api()
 vk.wall.post(message='Аккаунт  взломан! Ответственность взял анонимный хакер')
-for var in range(5):
-time.sleep(3)
 vk.wall.post(message='vto.pe')             
 print("[log] Сообщение отправленно. Ожидайте бана!")
 os.system("clear")
