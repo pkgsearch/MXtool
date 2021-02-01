e9b4ce47895e6ef5c85447ae73e7f90c21ae0fb1
pkgsearch <73498800+pkgsearch@users.noreply.github.com>
2021-02-01 15:26:32 +0300

    Create W.py

---
+++ core/W.py
@@ -0,0 +1,11 @@
+print("""
+m     m
+#  #  #  mmm    mmm   mmmm   mmmm
+" #"# # #   "  "   #  #" "#  #" "#
+ ## ##"  """m  m"""#  #   #  #   #
+ #   #  "mmm"  "mm"#  ##m#"  ##m#"
+                      #      #
+                      "      "
+""")
+w=input(" Введите номер +")
+print("https://api.whatsapp.com/send?phone="+w)
