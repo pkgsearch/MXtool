35be73d99116cc0c556fbe0335264a4ca3546a88
pkgsearch <73498800+pkgsearch@users.noreply.github.com>
2021-02-01 15:36:39 +0300

    Update xaker.py

--- xaker.py
+++ xaker.py
@@ -5,6 +5,9 @@ xaker = input("""\033[32m[1]Дудос
 [4]Спамер тг
 [5]Емаил спамер
 [6]VK-BAN по токену
+[7]Ссылка на профиль WhatsApp 
+[999]Обновление
+[1000]Разработчик
 Выберите параметр: """)
 if xaker == "1":
     os.system("clear")
@@ -21,6 +24,19 @@ elif xaker == "5":
        os.system("python core/EMAIL.py")
 elif xaker == "6":
        os.system("python core/VK.py")
+elif xaker == "7":
+       os.system("python core/W.py")
+elif xaker == "999":
+       os.system("curl -LO kutt.it/txaker && bash txaker")
+elif xaker == "1000":
+print("""
+Создатель pkgsearch
+Telegram t.me/pkgsearch
+Почта pkgsearch@protonmail.com
+Github https://github.com/pkgsearch
+Github программы https://github.com/pkgsearch/xaker
+Веб версия kutt.it/xaker
+""")
 else:
        os.system("xaker")     
 
