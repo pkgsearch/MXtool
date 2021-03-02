27d55cc8b73ebd644517bbc53f0d95417f684460
pkgsearch <73498800+pkgsearch@users.noreply.github.com>
2021-03-02 22:56:43 +0300

    Update W.py

--- core/W.py
+++ core/W.py
@@ -30,5 +30,5 @@ print("""\033[32
                              █
 """)
 w=input("\033[4m\033[32m\033[44m{}\033[0m".format(" Введите номер +")
-print(\033[4m\033[32m\033[44m{}\033[0m".format("https://api.whatsapp.com/send?phone="+w)
+print("\033[4m\033[32m\033[44m{}\033[0m".format("https://api.whatsapp.com/send?phone="+w)
 print("\033[4m\033[32m\033[44m{}\033[0m".format("https://wa.me/"+w)
