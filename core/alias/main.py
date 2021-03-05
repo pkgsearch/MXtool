043d97820ed6e381a2160e86ca5e79eaf8c36ecc
pkgsearch <73498800+pkgsearch@users.noreply.github.com>
2021-03-05 20:22:05 +0300

    Create main.py

---
+++ core/alias/main.py
@@ -0,0 +1,12 @@
+import is
+print("""Какая ваша os?
+[1]Android
+[2]Linux""")
+os=input(":")
+if os == "1":
+os.system("python ALIAS.py")
+elif is == "2":
+os.system("python ALIASL.py")
+else:
+os.system("python main.py")
+
