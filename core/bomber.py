d536afdbfdd6107c12b5b04f326e1f853661600d
pkgsearch <73498800+pkgsearch@users.noreply.github.com>
2021-12-12 21:16:32 +0300

    Update bomber.py

--- core/bomber.py
+++ core/bomber.py
@@ -130,48 +130,11 @@ for x in range(12):
     )
 
 _phone9 = _phone[1:]
-_phoneAresBank = (
-    "+"
-    + _phone[0]
-    + "("
-    + _phone[1:4]
-    + ")"
-    + _phone[4:7]
-    + "-"
-    + _phone[7:9]
-    + "-"
-    + _phone[9:11]
-)
-_phone9dostavista = (
-    _phone9[:3] + "+" + _phone9[3:6] + "-" + _phone9[6:8] + "-" + _phone9[8:10]
-)
-_phoneOstin = (
-    "+"
-    + _phone[0]
-    + "+("
-    + _phone[1:4]
-    + ")"
-    + _phone[4:7]
-    + "-"
-    + _phone[7:9]
-    + "-"
-    + _phone[9:11]
-)
-_phonePizzahut = (
-    "+"
-    + _phone[0]
-    + " ("
-    + _phone[1:4]
-    + ") "
-    + _phone[4:7]
-    + " "
-    + _phone[7:9]
-    + " "
-    + _phone[9:11]
-)
-_phoneGorzdrav = (
-    _phone[1:4] + ") " + _phone[4:7] + "-" + _phone[7:9] + "-" + _phone[9:11]
-)
+_phoneAresBank = '+'+_phone[0]+'('+_phone[1:4]+')'+_phone[4:7]+'-'+_phone[7:9]+'-'+_phone[9:11]
+_phone9dostavista = _phone9[:3]+'+'+_phone9[3:6]+'-'+_phone9[6:8]+'-'+_phone9[8:10]
+_phoneOstin = '+'+_phone[0]+'+('+_phone[1:4]+')'+_phone[4:7]+'-'+_phone[7:9]+'-'+_phone[9:11]
+_phonePizzahut = '+'+_phone[0]+' ('+_phone[1:4]+') '+_phone[4:7]+' '+_phone[7:9]+' '+_phone[9:11]
+_phoneGorzdrav = _phone[1:4]+') '+_phone[4:7]+'-'+_phone[7:9]+'-'+_phone[9:11]
 s = 0
 iteration = 0
 
